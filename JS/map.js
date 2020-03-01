b2e206c5e4ba83b4c081a98bd3db43cef1936849
lolo-portable <loris.pistilli.iut@gmail.com>
2020-03-01 13:45:48 +0100

    refonte de l'archotecture css et html et modification de la methode recherche avec l'api pour ne pas être obliger de choisir forecement les trois cas je fais un nouveau traitement des résultat et j'affiche enfin les reusltat dans une liste a la gauche de mon écran

--- JS/map.js
+++ JS/map.js
@@ -1,4 +1,4 @@
-var mymap = L.map('mapid').setView([51.505, -0.09], 13);
+var mymap = L.map('mapid').setView([48.864716, 2.349014], 12);
 L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
--- index.php
+++ index.php
@@ -1,10 +1,8 @@
 <?php
-include "data/phpdata/départementfr.php";
-
-
 $urldep = "https://data.enseignementsup-recherche.gouv.fr/api/records/1.0/search/?dataset=fr-esr-principaux-diplomes-et-formations-prepares-etablissements-publics&rows=0&sort=-rentree_lib&facet=dep_etab_lib&refine.rentree_lib=2017-18";
 $urldip = "https://data.enseignementsup-recherche.gouv.fr/api/records/1.0/search/?dataset=fr-esr-principaux-diplomes-et-formations-prepares-etablissements-publics&rows=0&sort=-rentree_lib&facet=diplome_rgp&refine.rentree_lib=2017-18";
 $urlfil = "https://data.enseignementsup-recherche.gouv.fr/api/records/1.0/search/?dataset=fr-esr-principaux-diplomes-et-formations-prepares-etablissements-publics&rows=0&sort=-rentree_lib&facet=discipline_lib&refine.rentree_lib=2017-18";
+
 //mes département
 $json_content_dep = file_get_contents($urldep);
 $json_dep = json_decode($json_content_dep);
@@ -27,102 +25,146 @@ if (isset($_POST["formbutton"])) {
     $post_diplome = $_POST["diplome"];
     $post_filiere = $_POST["filiere"];
     $post_departement = $_POST["departement"];
-    $request = "https://data.enseignementsup-recherche.gouv.fr/api/records/1.0/search/?dataset=fr-esr-principaux-diplomes-et-formations-prepares-etablissements-publics&rows=10&facet=etablissement_lib&refine.dep_etab_lib=" . $post_departement . "&refine.diplome_rgp=" . $post_diplome . "&refine.discipline_lib=" . $post_filiere;
+    $request = "https://data.enseignementsup-recherche.gouv.fr/api/records/1.0/search/?dataset=fr-esr-principaux-diplomes-et-formations-prepares-etablissements-publics&facet=etablissement_lib";
+    if (!empty($post_departement)) {
+        $request = $request . "&refine.dep_etab_lib=" . $post_departement;
+    } else if (!empty($post_diplome)) {
+        $request = $request . "&refine.diplome_rgp=" . $post_diplome;
+    } else if (!empty($post_filiere)) {
+        $request = $request . "&refine.discipline_lib=" . $post_filiere;
+    } else {
+        $request = "";
+        $form = false;
+    }
     $json_content_request = file_get_contents($request);
     $json_request = json_decode($json_content_request);
-    $facets_request = $json_request->facet_groups[2]->facets;
+    $record_request = $json_request->records;
+    $IDs = array();
+    $myschoolsname = array();
+    foreach ($record_request as $school) {
+        array_unique($IDs);
+        if (!(in_array($school->fields->etablissement, $IDs))) {
+            $IDs[] = $school->fields->etablissement;
+            $myschoolsname[] = $school->fields->etablissement_lib;
+        }
+    }
 }
 ?>
-<!DOCTYPE html>
-<html dir="ltr" lang="fr">
+<!doctype html>
+<html lang="fr">
 <head>
-    <link href="css/style.css" rel="stylesheet">
+    <meta charset="UTF-8">
+    <title>Bac+x</title>
+    <link rel="stylesheet" href="css/finalstyle.css">
+
+    <!--old-->
     <link rel="stylesheet" href="https://unpkg.com/leaflet@1.6.0/dist/leaflet.css"
           integrity="sha512-xwE/Az9zrjBIphAcBb3F6JVqxf46+CDLwfLMHloNu6KEQCAWi6HcDUbeOfBIptF7tcCzusKFjFw2yuvEpDL9wQ=="
           crossorigin=""/>
     <meta charset="utf-8">
-    <title>Projet temporairement sans nom</title>
+    <title>BAC+x</title>
     <script src="JS/jquery-3.4.1.min.js"></script>
     <script src="https://unpkg.com/leaflet@1.6.0/dist/leaflet.js"
             integrity="sha512-gZwIG9x3wUXg2hdXF6+rVkLF/0Vi9U8D2Ntg4Ga5I5BZpVkVxlJWbSQtXPSiUTtC0TjtGOmxa1AJPuV0CPthew=="
             crossorigin=""></script>
+    <!--    ----->
+
 </head>
 <body>
-<div class="main_menu" id="panel-inner">
-    <nav>
-        <ul>
-            <form action="index.php" method="POST" name="form">
-                <select class="select" placeholder="votre diplome" name="diplome">
-                    <option value="" disabled selected>-- votre diplome --</option>
-                    <?php
-                    foreach ($facets_dip as $facet) {
-                        $data = $facet->path;
-                        print "<option value=\"" . $data . "\">" . $data . "</option>";
-                    }
+<div class="box">
+    <div class="row header">
+        <nav>
+            <ul>
+                <form action="index.php" method="POST" name="form">
+                    <select class="select" placeholder="votre diplome" name="diplome">
+                        <option value="" selected>-- votre diplome --</option>
+                        <?php
+                        foreach ($facets_dip as $facet) {
+                            $data = $facet->path;
+                            print "<option value=\"" . $data . "\">" . $data . "</option>";
+                        }
 
-                    ?>
-                </select>
-                <select class="select" placeholder="votre departement" name="departement">
-                    <option value="" disabled selected>-- votre departement --</option>
-                    <?php
-                    foreach ($facets_dep as $facet) {
-                        $data = $facet->path;
-                        print "<option value=\"" . $data . "\">" . $data . "</option>";
-                    }
-                    ?>
-                </select>
-                <select class="select" placeholder="votre filiere" name="filiere">
-                    <option value="" disabled selected>-- votre filiere --</option>
-                    <?php
-                    foreach ($facets_fil as $facet) {
-                        $data = $facet->path;
-                        print "<option value=\"" . $data . "\">" . $data . "</option>";
-                    }
+                        ?>
+                    </select>
+                    <select class="select" placeholder="votre departement" name="departement">
+                        <option value="" selected>-- votre departement --</option>
+                        <?php
+                        foreach ($facets_dep as $facet) {
+                            $data = $facet->path;
+                            print "<option value=\"" . $data . "\">" . $data . "</option>";
+                        }
+                        ?>
+                    </select>
+                    <select class="select" placeholder="votre filiere" name="filiere">
+                        <option value="" selected>-- votre filiere --</option>
+                        <?php
+                        foreach ($facets_fil as $facet) {
+                            $data = $facet->path;
+                            print "<option value=\"" . $data . "\">" . $data . "</option>";
+                        }
 
-                    ?>
-                </select>
-                <input type="submit" value="rechercher" name="formbutton">
-            </form>
-        </ul>
-        <ul>
-            <li>
-                <form action="index.php" class="" method="post">
-                    <div class="slidecontainer">
-                        <h2>Bac+: <span id="output"></span></h2>
-                        <label for="myRange"></label>
-                        <input class="slider" id="myRange" max="8" min="1" type="range" value="0">
-                    </div>
+                        ?>
+                    </select>
+                    <input type="submit" value="rechercher" name="formbutton">
                 </form>
-            </li>
-        </ul>
-    </nav>
-</div>
-<div class="mySchools">
-    <?php
-    if ($form) {
-        if (isset($facets_request)) {
-            echo "nous avons trouvé " . sizeof($facets_request) . " résultat(s) en " . $post_filiere . " dans le département de " . $post_departement . " pour un " . $post_diplome . "<br><br>";
-            foreach ($facets_request as $school) {
-                echo ">>>" . $school->{'name'} . "<<<" . "<br>";
-            }
-        } else {
-            echo "nous n'avons trouvé aucun résultat en " . $post_filiere . " dans le département de " . $post_departement . " pour un " . $post_diplome . "<br><br>";
+            </ul>
+        </nav>
+    </div>
+    <div class="row content">
+        <?php
+        if ($form) {
+            ?>
+            <div class="mySchools">
+                <?php
+                if (isset($record_request)) {
+                    ?>
+                    <div class="infobox infobox-2">
+                        <?php
+                        echo "<p>nous avons trouvé " . sizeof($myschoolsname) . " résultat(s) en " . $post_filiere . " dans le département de " . $post_departement . " pour un " . $post_diplome . "</p>";
+                        ?>
+                    </div>
+                    <div class="mySchoolsinner">
+                        <?php
+                        foreach ($myschoolsname as $school) {
+                            ?>
+                            <div class="infobox infobox-1">
+                                <?php
+                                echo "<p>" . $school . "</p>";
+                                ?>
+                            </div>
+                            <?php
+                        }
+                        ?>
+                    </div>
+                    <?php
+                } else {
+                    ?>
+                    <div class="infobox">
+                        <?php
+                        echo "nous n'avons trouvé aucun résultat en " . $post_filiere . " dans le département de " . $post_departement . " pour un " . $post_diplome . "<br>";
+                        ?>
+                    </div>
+                    <?php
+
+                }
+                ?>
+            </div>
+            <?php
         }
-    }
-    ?>
+        ?>
+
+        <div id="mapcontainer" style="width: <?php echo $form ? '80%' : '100%'; ?>">
+            <div id="mapid"></div>
+        </div>
+    </div>
+    <footer class="row footer">
+        <nav>
+            <ul class="importantlinks">
+                <li><a href="https://github.com/lolozeeniboss/opendata_bacX.git">en savoir plus(GitHub)</a></li>
+            </ul>
+        </nav>
+    </footer>
 </div>
-<div id="mapcontainer">
-    <div id="mapid"></div>
-</div>
-<footer>
-    <nav>
-        <ul class="importantlinks">
-            <li><a href="https://github.com/lolozeeniboss/opendata_bacX.git">en savoir plus(GitHub)</a></li>
-        </ul>
-    </nav>
-</footer>
-<script src="JS/slider.js"></script>
-<script src="JS/showAndHideMenu.js"></script>
 <script src="JS/map.js"></script>
 </body>
 </html>
